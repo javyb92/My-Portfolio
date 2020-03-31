7c6cda8fe199e10bd2fa0ae21d2f1e59fe32a7c0
jab_tx <javier.a.baragan92@gmail.com>
2020-03-31 06:38:31 -0500

    text adjustments

--- src/Pages/Greeting.js
+++ src/Pages/Greeting.js
@@ -6,7 +6,7 @@ function Greeting() {
         <div className="jumbotron jumbotron-fluid col-lg-10" id="greeting">   
             <div className="greeting-text">
                 <h1>JAVIER A. BARRAGAN</h1>
-                <h4>Placeholder Text</h4>
+                <p className="statement">I'm a Dallas based Web Developer, with the strongest desire to learn and produce.</p>
             </div>
         </div>
         );
