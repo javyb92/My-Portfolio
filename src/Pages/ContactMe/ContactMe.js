fff0ea486bfa619edb0ad1ee7f34a0228ff88d71
jab_tx <javier.a.baragan92@gmail.com>
2020-04-12 20:09:24 -0500

    added form functionality use netlify

--- src/Pages/ContactMe/ContactMe.js
+++ src/Pages/ContactMe/ContactMe.js
@@ -1,26 +1,61 @@
 import React from "react";
 import "./style.css";
 
-function ContactMe() {
-    return (
-        <section className="container col-10">
+const encode = (data) => {
+    return Object.keys(data)
+        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+        .join("&");
+  }
+
+  class ContactForm extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { name: "", email: "", message: "" };
+    }
+
+    /* Here’s the juicy bit for posting the form submission */
+
+    handleSubmit = e => {
+      fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: encode({ "form-name": "contact", ...this.state })
+      })
+        .then(() => alert("Success!"))
+        .catch(error => alert(error));
+
+      e.preventDefault();
+    };
+
+    handleChange = e => this.setState({ [e.target.name]: e.target.value });
+
+
+    render() {
+        const { name, email, message } = this.state;
+        return (
+            <section className="container col-10">
             <div className="contact-me">
                 <h5>Contact Me!</h5>
-                <form name="contact" action="POST" data-netlify="true" >
-                    <div className="labels">
-                        <label>Your Name: <input type="text" name="name" /></label>
-                        <label>Your Email: <input type="email" name="email"/></label>   
-                    </div>
-                    <label>Message: </label>
-                    <div>
-                        <textarea name="message" placeholder="Your message...."></textarea>
-                    </div>
-                    <div>
+                <form onSubmit={this.handleSubmit}>
+                    <p>
+                        <label>
+                        Your Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
+                        </label>
+                        <label>
+                        Your Email: <input type="email" name="email" value={email} onChange={this.handleChange} />
+                        </label>
+                    </p>
+                    <p>
+                            <textarea name="message" value={message} onChange={this.handleChange}/>
+                    </p>
+                    <p>
                         <button type="submit">Send</button>
-                    </div>
-                </form>
+                    </p>
+                    </form>
             </div>
         </section>
+          
         );
-    };
-export default ContactMe;
+    }
+}
+export default ContactForm;
