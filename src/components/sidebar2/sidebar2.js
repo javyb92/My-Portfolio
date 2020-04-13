c8ff1ed418dc46e28b88859957a854038622edc8
jab_tx <javier.a.baragan92@gmail.com>
2020-04-12 19:15:52 -0500

    fixed glitch where hovering over hidden toggle button in desktop mode breaks sidebar

--- src/components/sidebar2/sidebar2.js
+++ src/components/sidebar2/sidebar2.js
@@ -14,37 +14,37 @@ function Sidebar2() {
                         <img className="logo" src={icon}></img>
                     </Link>
                 </li>
-                <li className="nav-item">
-                    <Link to="about" className="nav-link" smooth={true} offset={-145} duration={1000}>
-                        <i className="fas fa-id-card"></i><span className="link-text"> ABOUT ME</span>
-                    </Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="portfolio" className="nav-link" smooth={true} offset={-50}  duration={1000}>
-                        <i className="fas fa-file-code"></i> <span className="link-text"> PORTFOLIO</span>
-                    </Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="contact-me" className="nav-link" smooth={true} duration={1000}>
-                        <i className="fas fa-envelope"></i> <span className="link-text"> CONTACT ME</span>
-                    </Link>
-                </li>
-                {/* <li class="nav-item">
-                    <a href="https://github.com/javyb92" class="nav-link">
-                        <i className="fab fa-github-square"></i><span class="link-text"> GITHUB</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="https://www.linkedin.com/in/javierabarragan92/" class="nav-link">
-                        <i className="fab fa-linkedin"></i><span class="link-text"> LINKEDIN</span>
-                    </a>
-                </li> */}
-                <li className="nav-item">
-                    <a href="https://drive.google.com/file/d/1xv5GSXcNAs5A7eWuMEvnwZMPBAvodKNE/view?usp=sharing" className="nav-link">
-                    <i className="fas fa-journal-whills"></i><span className="link-text"> RESUME</span>
-                    </a>
-                </li>
-                <li className="nav-item" id="last">
+                    <li className="nav-item">
+                        <Link to="about" className="nav-link" smooth={true} offset={-145} duration={1000}>
+                            <i className="fas fa-id-card"></i><span className="link-text"> ABOUT ME</span>
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="portfolio" className="nav-link" smooth={true} offset={-50}  duration={1000}>
+                            <i className="fas fa-file-code"></i> <span className="link-text"> PORTFOLIO</span>
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="contact-me" className="nav-link" smooth={true} duration={1000}>
+                            <i className="fas fa-envelope"></i> <span className="link-text"> CONTACT ME</span>
+                        </Link>
+                    </li>
+                    {/* <li class="nav-item">
+                        <a href="https://github.com/javyb92" class="nav-link">
+                            <i className="fab fa-github-square"></i><span class="link-text"> GITHUB</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="https://www.linkedin.com/in/javierabarragan92/" class="nav-link">
+                            <i className="fab fa-linkedin"></i><span class="link-text"> LINKEDIN</span>
+                        </a>
+                    </li> */}
+                    <li className="nav-item">
+                        <a href="https://drive.google.com/file/d/1xv5GSXcNAs5A7eWuMEvnwZMPBAvodKNE/view?usp=sharing" className="nav-link">
+                        <i className="fas fa-journal-whills"></i><span className="link-text"> RESUME</span>
+                        </a>
+                    </li>
+                    <li className="nav-item" id="last">
                     <a href="#" className="nav-link">
                         <i className="fas fa-caret-square-right" id="toggle"></i>
                         <i className="fas fa-caret-square-left" id="toggle2"></i>
