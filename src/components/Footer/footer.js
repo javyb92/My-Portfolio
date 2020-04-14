e098e76ddec05a20784daa953ed6edea1f534b6d
jab_tx <javier.a.barragan92@gmail.com>
2020-04-14 12:27:49 -0500

    adjusted footer

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./style.css";
+import reacticon from "../../img/icons/react.png"
+import bootstrapicon from "../../img/icons/bootstrap.png"
+
 
 function Footer() {
     return (
         <footer className="container col-12">
             <div className="content">
-                <h5>Design and made with ❤️ by Javier A. Barragan</h5>
+                <h5>Created with <img src={reacticon} width="16" alt="React.js"/> <img src={bootstrapicon} width="20" alt="Bootstrap4"/> by Javier A. Barragan</h5>
                 <div className="social-media">
                     <a href="https://github.com/javyb92" className="footer-link">
                         <i className="fab fa-github-square footer-icon"></i>
